73ab12cbf06bf15e2c7458997ee67e7b3d3bb6fb
nolaneo <eoin.nolan@intercom.io>
2019-12-27 16:09:32 +0000

    add deployment

---
+++ config/deploy.js
@@ -0,0 +1,11 @@
+module.exports = function() {
+  let ENV = {
+    build: {},
+    rootURL: "/seconds/",
+    locationType: "hash",
+    ghpages: {
+      gitRemoteUrl: "git@github.com:/nolaneo/seconds"
+    }
+  };
+  return ENV;
+};
