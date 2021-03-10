fac1f89aeb7540de546000021dbcf6a085ed90b6
nolaneo <eoin.nolan@intercom.io>
2021-03-10 22:25:53 +0000

    reset

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,47 +7,50 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
     ecmaFeatures: {
-      legacyDecorators: true,
-    },
+      legacyDecorators: true
+    }
   },
-  plugins: ['ember'],
+  plugins: [
+    'ember'
+  ],
   extends: [
     'eslint:recommended',
-    'plugin:ember/recommended',
-    'plugin:prettier/recommended',
+    'plugin:ember/recommended'
   ],
   env: {
-    browser: true,
+    browser: true
   },
-  rules: {},
+  rules: {
+    'ember/no-jquery': 'error'
+  },
   overrides: [
     // node files
     {
       files: [
         '.eslintrc.js',
-        '.prettierrc.js',
         '.template-lintrc.js',
         'ember-cli-build.js',
         'testem.js',
         'blueprints/*/index.js',
         'config/**/*.js',
         'lib/*/index.js',
-        'server/**/*.js',
+        'server/**/*.js'
       ],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: 'script'
       },
       env: {
         browser: false,
-        node: true,
+        node: true
       },
       plugins: ['node'],
-      extends: ['plugin:node/recommended'],
-      rules: {
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+
         // this can be removed once the following is fixed
         // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        'node/no-unpublished-require': 'off',
-      },
-    },
-  ],
+        'node/no-unpublished-require': 'off'
+      })
+    }
+  ]
 };
--- app/router.js
+++ app/router.js
@@ -1,9 +1,10 @@
 import EmberRouter from '@ember/routing/router';
-import config from 'seconds/config/environment';
+import config from './config/environment';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function() {
+});
