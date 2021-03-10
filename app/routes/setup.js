bc03027953483b1686c7727d34e6c9831c006393
nolaneo <eoin.nolan@intercom.io>
2021-03-10 21:39:10 +0000

    reset

--- app/routes/setup.js
+++
@@ -1,7 +0,0 @@
-import Route from "@ember/routing/route";
-
-export default class SetupRoute extends Route {
-  beforeModel() {
-    window.scrollTo(0, 1);
-  }
-}
--- app/services/state.js
+++
@@ -1,6 +0,0 @@
-import Service from "@ember/service";
-import { tracked } from "@glimmer/tracking";
-
-export default class StateService extends Service {
-  @tracked teams = 0;
-}
