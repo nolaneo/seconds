ed86148c1d9d1d40ca6d0e3d3eeea322b0563e49
nolaneo <eoin.nolan@intercom.io>
2021-03-11 21:22:32 +0000

    working game

--- app/components/game.js
+++ app/components/game.js
@@ -5,12 +5,15 @@ import { timeout } from 'ember-concurrency';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import { set } from '@ember/object';
 
-function shuffleArray(array) {
+function shuffleArray(input) {
+  let array = [...input];
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
+  return array;
 }
 
 export default class GameComponent extends Component {
@@ -20,21 +23,51 @@ export default class GameComponent extends Component {
   @tracked time;
   @tracked playerReady = false;
   @tracked showScoreSelect = false;
+  @tracked waiting = false;
+  @tracked scores = [];
 
   constructor() {
     super(...arguments);
     this.shuffledWords = shuffleArray(words);
     this.gameService.currentPlayer = 0;
+    this.scores = new Array(this.gameService.numberOfPlayers).fill(0);
+    this.waiting = true;
   }
 
   @task *countdown() {
-    for (let i = 30; i > 0; --i) {
+    document.querySelector('audio')?.play();
+    for (let i = 2; i > 0; --i) {
       this.time = i;
       yield timeout(1000);
     }
+    this.showScoreSelect = true;
   }
 
-  @action addScore(score) {
-    console.log(score);
+  @action ready() {
+    this.card = [];
+    for (let i = 0; i < 5 && i < this.shuffledWords.length; ++i) {
+      this.card.push({ word: this.shuffledWords.pop(), correct: false });
+    }
+    this.waiting = false;
+    this.showScoreSelect = false;
+    this.countdown.perform();
+  }
+
+  @action addScore() {
+    let score = this.card.filter((card) => card.correct).length;
+    this.scores[this.gameService.currentPlayer] += score;
+    this.nextPlayer();
+  }
+
+  @action toggleScore(item) {
+    set(item, 'correct', !item.correct);
+  }
+
+  nextPlayer() {
+    this.gameService.currentPlayer += 1;
+    this.gameService.currentPlayer =
+      this.gameService.currentPlayer % this.gameService.numberOfPlayers;
+    this.waiting = true;
+    this.showScoreSelect = false;
   }
 }
--- app/routes/game/in-progress.js
+++ app/routes/game/in-progress.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class GameInProgressRoute extends Route {
+  @service gameService;
+
+  beforeModel() {
+    if (isNaN(this.gameService.numberOfPlayers)) {
+      this.transitionTo('game.player-select');
+    }
+  }
 }
